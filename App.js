b434bbb53b50df9b7b0c65a74eb8ce5b7c598831
Kirill Fedyakov <vogelfenix@gmail.com>
2021-12-18 20:11:18 +0100

    Update main App Component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import StreakBar from './components/streak-bar/StreakBar';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Demo</Text>
+      <StreakBar />
     </View>
   );
 }
@@ -14,8 +15,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+
+    paddingTop: Constants.statusBarHeight + 20,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+
+    // backgroundColor: '#fffff4',
+    backgroundColor: '#f6ffe6',
   },
 });
