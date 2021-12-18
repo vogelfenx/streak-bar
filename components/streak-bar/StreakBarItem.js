499e6dec1f067b3d78ec0f1997fd6060be39a60e
Kirill Fedyakov <vogelfenix@gmail.com>
2021-12-18 16:28:34 +0100

    Add StreakBarItem component skeleton

---
+++ components/streak-bar/StreakBarItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import Circle from '../common/Circle'
+
+export default function StreakBarItem(props) {
+  return (
+    <Circle />
+  )
+}
